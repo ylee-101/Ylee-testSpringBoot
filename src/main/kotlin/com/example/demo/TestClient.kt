0f9531b588a01d0890210a067be3ab02bb4f2933
ylee <ylee.develop@gmail.com>
2026-09-10 14:48:33 +0900

    baseurl 처리 리팩토링

--- src/main/kotlin/com/example/demo/TestClient.kt
+++ src/main/kotlin/com/example/demo/TestClient.kt
@@ -8,16 +8,17 @@ import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
 
 @Component
-class TestClient(
-    private val webClient : WebClient = WebClient.builder().build()
-) {
+class TestClient {
     private val TAG = "TestClient"
 
+    private val webClient : WebClient = WebClient.builder()
+        .baseUrl("http://localhost:9090")
+        .build()
+
     fun getStream(): Flux<ServerSentEvent<String>> {
-        val baseUrl = "http://localhost:9090"
         AppLogger.info(TAG, "getStream")
         return webClient.get()
-            .uri(baseUrl+"/mock/stream?scenario=slow-response")
+            .uri("/mock/stream?scenario=slow-response")
             .retrieve()
             .bodyToFlux(
                 object : ParameterizedTypeReference<ServerSentEvent<String>>() {}
