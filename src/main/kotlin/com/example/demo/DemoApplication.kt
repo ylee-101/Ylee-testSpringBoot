6e54e64c99697852cd994ad40eb48ff9581c1969
ylee <ylee.develop@gmail.com>
2026-09-15 16:10:17 +0900

    외부api 연결 환경설정 bean관리되도록 어노테이션 추가

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,9 +1,11 @@
 package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class DemoApplication
 
 fun main(args: Array<String>) {
