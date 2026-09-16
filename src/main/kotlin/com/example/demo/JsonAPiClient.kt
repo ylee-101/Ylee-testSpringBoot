d38f8fea74b3ed04262d5bd0e5a520dbe93631ea
ylee <ylee.develop@gmail.com>
2026-09-16 15:07:42 +0900

    추가 외부 api(9091포트) 용 클라이언트 추가

---
+++ src/main/kotlin/com/example/demo/JsonAPiClient.kt
@@ -0,0 +1,44 @@
+package com.example.demo
+
+import com.example.demo.common.AppLogger
+import com.example.demo.config.ExternalApiProperties
+import com.example.demo.config.JsonPortProperties
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.ResponseEntity
+import org.springframework.http.codec.ServerSentEvent
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import tools.jackson.databind.JsonNode
+
+@Component
+class JsonAPiClient(
+    private val externalApiProperties: ExternalApiProperties
+) {
+    private val TAG = "JsonAPiClient"
+
+    private val jsonApiWebClient : WebClient = WebClient.builder()
+        .baseUrl(externalApiProperties.jsonPort.baseUrl)
+        .build()
+
+    fun getJson(): Mono<ResponseEntity<MockJsonResponse>>{
+        AppLogger.info(TAG, "requesting json api")
+
+        return jsonApiWebClient.get()
+            .uri { builder ->
+                builder.path(externalApiProperties.jsonPort.path)
+                    .build()
+            }
+            .retrieve()
+            .toEntity(MockJsonResponse::class.java)
+    }
+}
+
+
+data class MockJsonResponse(
+    val source: String,
+    val scenario: String,
+    val success: Boolean,
+    val message: String,
+)
